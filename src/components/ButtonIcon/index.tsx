b43f060741c447b2709885e7fc6bbd841379be82
sankeyangshu <1260323835@qq.com>
2025-05-15 21:12:38 +0800

    feat: full screen

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,94 @@
+import { Button, Tooltip, type ButtonProps, type TooltipProps } from 'antd';
+import Icon from '../Icon';
+import type { CSSProperties, ReactNode } from 'react';
+
+interface Props extends Omit<ButtonProps, 'icon' | 'iconPosition'> {
+  /**
+   * children (子元素)
+   */
+  children?: ReactNode;
+  /**
+   * Button class (按钮类名)
+   */
+  className?: string;
+  /**
+   * Iconify icon name (图标名称)
+   */
+  icon?: string;
+  /**
+   * local svg icon (本地svg图标)
+   */
+  localIcon?: string;
+  /**
+   * style (样式)
+   */
+  style?: CSSProperties;
+  /**
+   * Tooltip content (提示内容)
+   */
+  tooltipContent?: string;
+  /**
+   * Tooltip placement (提示位置)
+   */
+  tooltipPlacement?: TooltipProps['placement'];
+  /**
+   * Trigger tooltip on parent (浮层渲染父节点)
+   */
+  triggerParent?: boolean;
+  /**
+   * z-index (层级)
+   */
+  zIndex?: number;
+}
+
+// 动态计算class
+const computeClass = (className: string) => {
+  let classStr = className;
+
+  if (!classStr.includes('h-')) {
+    classStr += ' h-36px';
+  }
+
+  if (!classStr.includes('text-')) {
+    classStr += ' text-icon';
+  }
+
+  return classStr;
+};
+
+const ButtonIcon = ({
+  children,
+  className = 'h-36 text-icon',
+  icon,
+  localIcon,
+  style,
+  tooltipContent,
+  tooltipPlacement = 'bottom',
+  triggerParent,
+  zIndex = 98,
+  ...props
+}: Props) => {
+  const cls = computeClass(className);
+
+  // 获取浮层渲染父节点
+  const getPopupContainer = (dom: HTMLElement) => {
+    return triggerParent ? dom.parentElement! : document.body;
+  };
+
+  return (
+    <Tooltip
+      placement={tooltipPlacement}
+      title={tooltipContent}
+      zIndex={zIndex}
+      getPopupContainer={getPopupContainer}
+    >
+      <Button type="text" className={cls} {...props}>
+        <div className="flex-center gap-8">
+          {children || <Icon icon={icon} localIcon={localIcon} style={style} />}
+        </div>
+      </Button>
+    </Tooltip>
+  );
+};
+
+export default ButtonIcon;
--- src/layouts/components/Header/components/ScreenFull.tsx
+++ src/layouts/components/Header/components/ScreenFull.tsx
@@ -1,56 +1,15 @@
-import { FullscreenExitOutlined, FullscreenOutlined } from '@ant-design/icons';
-import { useBoolean } from 'ahooks';
-import { message, Tooltip } from 'antd';
-import { createElement, useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
-import screenfull from 'screenfull';
+import { useFullscreen } from 'ahooks';
+import ButtonIcon from '@/components/ButtonIcon';
 
 const ScreenFull = () => {
-  // 使用i18n全局函数
-  const { t } = useTranslation();
-
-  // 是否全屏和切换方法
-  const [isFullscreen, { setTrue, setFalse }] = useBoolean(screenfull.isFullscreen);
-
-  // 全屏切换
-  const onClickToggleScreenFull = () => {
-    if (!screenfull.isEnabled) {
-      message.warning('当前您的浏览器不支持全屏 ❌');
-    }
-    screenfull.toggle();
-  };
-
-  // 监听 screenfull 变化
-  const changeScreenFull = () => {
-    if (screenfull.isFullscreen) setTrue();
-    else setFalse();
-  };
-
-  useEffect(() => {
-    // 组件挂载时调用
-    if (screenfull.isEnabled) {
-      // 设置侦听器
-      screenfull.on('change', changeScreenFull);
-    }
-
-    return () => {
-      // 组件卸载时调用
-      if (screenfull.isEnabled) {
-        // 删除侦听器
-        screenfull.off('change', changeScreenFull);
-      }
-    };
-  }, []);
+  const [isFullscreen, { toggleFullscreen }] = useFullscreen(document.body);
 
   return (
-    <>
-      <Tooltip title={!isFullscreen ? t('navBar.screenfull') : t('navBar.screenfullRetract')}>
-        {createElement(isFullscreen ? FullscreenExitOutlined : FullscreenOutlined, {
-          className: 'icon-style',
-          onClick: onClickToggleScreenFull,
-        })}
-      </Tooltip>
-    </>
+    <ButtonIcon
+      localIcon={isFullscreen ? 'exit-fullscreen' : 'fullscreen'}
+      className="px-12"
+      onClick={toggleFullscreen}
+    />
   );
 };
 
